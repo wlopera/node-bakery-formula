6e2a0caa23fc4d6d69e068e8f9adf5251e0736ee
William Lopera <con_wlopera@credicorpbank.com>
2022-12-27 14:43:25 -0500

    subir json para deploy en vercel-2

--- src/auth/index.js
+++ src/auth/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 module.exports.AuthAPI = (app) => {
   router
-    .get("/", AuthController.getUsers) // http"//localhost:8585/api/users/
+    //.get("/", AuthController.getUsers) // http"//localhost:8585/api/users/
     .post("/login", AuthController.login); // http"//<url>:<port>/api/user/login/
 
   app.use("/api/users", router);
